ff97acce381094ac73a4b1ea3b780085ee2ce706
Psalm de Mesa <demesap@wsc.school.nz>
2025-05-08 10:05:23 +1200

    General update

--- milestone_1/milestone_1_code.py
+++ milestone_1/milestone_1_code.py
@@ -1,6 +1,6 @@
 #Ask user for age and years to add
-current_age = int(input("How old are you now?"))
-years_to_add = int(input("How many years would you like to add?"))
+current_age = int(input("How old are you now? "))
+years_to_add = int(input("How many years would you like to add? "))
 #Calculate the total of age plus years to add
 current_age += years_to_add
 #Print out the total
