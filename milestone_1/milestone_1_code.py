d6dfa8f20387410f2a51c1377db6e9807f6eb7a8
Psalm de Mesa <demesap@wsc.school.nz>
2025-05-15 09:03:52 +1200

    Update milestone_1_code.py
    Added code asking for name and changed the final print.

--- milestone_1/milestone_1_code.py
+++ milestone_1/milestone_1_code.py
@@ -1,7 +1,9 @@
+#Ask user for name
+name = input("What is your name?")
 #Ask user for age and years to add
 current_age = int(input("How old are you now? "))
 years_to_add = int(input("How many years would you like to add? "))
 #Calculate the total of age plus years to add
 current_age += years_to_add
 #Print out the total
-print("In {} years you will be {}" .format(years_to_add,current_age))
+print("Hi {}! You will be {} in {} years." .format(name,current_age,years_to_add))
