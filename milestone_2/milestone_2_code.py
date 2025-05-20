a63cfe92527db1749a3eb892336dac28781d732d
Psalm de Mesa <demesap@wsc.school.nz>
2025-05-20 14:51:08 +1200

    Update milestone_2_code.py
    added comments

--- milestone_2/milestone_2_code.py
+++ milestone_2/milestone_2_code.py
@@ -1,20 +1,42 @@
 import re
+
 #Ask user for a password
 password = input("Enter a password:")
+
+
 #Counting and grouping the variables
 x = len(password)
-#Compare and print
+is_valid = True
+
+#Check password length
 if len(password) < 10:
     print("Password must have atleast 10 characters.")
+
+
+#Check for lowercase
 if not re.search (r"[a-z]", password):
     print("Password must have atleast 1 lowercase letter.")
+    is_valid = False
+
+#Check for uppercase
 if not re.search (r"[A-Z]", password):
     print("Password must have aleast 1 uppercase letter.")
+    is_valid = False
+
+#Check for numbers
 if not re.search (r"[\d]", password):
     print("password must have atleast 1 number.")
-if re.search(r"[!#$%^&*?]", password):
+    is_valid = False
+
+#Check for any invalid special character
+if not re.search(r"[@ . _]", password):
     print("Password must only contain any of these special characters: @ . _")
-if len(password) >= 10 and (r"[a-z]", password) and (r"[A-Z]", password) and (r"[\d]", password) and (r"[@ . _]", password):
+    is_valid = False
+
+#Final result if true
+if is_valid:
     print("Password is strong.")
+
+#Final result if false
 else:
     print("Password: \"{}\" is weak." .format(password))
